554bf9c58599669e577cd8d9fc8837791c20e7ed
madhankumar <madhankumar028@gmail.com>
2017-02-12 22:36:47 +0530

    added build command

---
+++ src/component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class HelloWorldComponent extends React.Component {
+  render() {
+    return (
+      <div> Hello world!! </div>
+    )
+  }
+}
+export default HelloWorldComponent;
